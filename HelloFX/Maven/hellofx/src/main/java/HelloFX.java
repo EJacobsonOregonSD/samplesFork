4660a7d30c746a4c7714da0e8d0a2b008d88085f
EJacobsonOregonSD <erik.jacobson@oregonsd.net>
2026-06-05 01:51:09 +0000

    idk what to name this

--- HelloFX/Maven/hellofx/src/main/java/HelloFX.java
+++ HelloFX/Maven/hellofx/src/main/java/HelloFX.java
@@ -7,19 +7,56 @@ import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.animation.AnimationTimer;
 
 public class HelloFX extends Application {
+
     Canvas canvas;
-    GraphicsContext graphics;
+    public int frame=0;
+    public double totalSimTime=0;
+    //private boolean running = true;
+
     public void start(Stage stage) {
         canvas=new Canvas(640,480);
-        graphics=canvas.getGraphicsContext2D();
-        graphics.setFill(Color.web("#0080FF"));
-        graphics.fillOval(100,100,50,50);
+
+        AnimationTimer timer = new AnimationTimer() {
+            private long lastTime = 0;
+            public void handle(long now) {
+                if (lastTime == 0) {
+                    lastTime = now;
+                    return;
+                }
+                double dt = (now - lastTime) / 1_000_000_000.0;
+                lastTime=now;
+                frame++;
+                draw();
+                // if(dt>(1.0/60)){
+                //     lastTime = now;
+                //     frame++;
+                //     draw();
+                // }
+                //if (running) {update(dt);}
+                
+            }
+        };
+
+        timer.start();
+
         Scene scene = new Scene(new StackPane(canvas), 640, 480);
         stage.setScene(scene);
         stage.setTitle("Gravity simulation");
         stage.show();
+    }
+
+    private void draw() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        gc.setFill(Color.web("#0080FF"));
+        gc.fillOval(100,100,50,50);
+        gc.fillText("Frames:"+frame,10,10);
     }
 
     public static void main(String[] args) {
